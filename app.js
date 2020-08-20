b886daf796d780210651419315d8dfa30fbc4efc
Jasmeet1718 <65601510+Jasmeet1718@users.noreply.github.com>
2020-08-20 16:03:59 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+
+$(".ibutton").click(function(){
+  $("html").toggleClass("invert")
+  $("img").toggleClass("invert")
+  $(".star").toggleClass("invert")
+  $(".ibutton").toggleClass("invert")
+});
